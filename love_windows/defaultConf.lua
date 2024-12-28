d247503b6ce335baeb5d455d0da50fa155550740
Milo:3 Silli Denote <71358745+MiloX3-Silli-Denote@users.noreply.github.com>
2024-12-28 10:19:48 -0500

    Update defaultConf.lua

--- love_windows/defaultConf.lua
+++ love_windows/defaultConf.lua
@@ -2,7 +2,7 @@ local DefaultConf = [[
 function love.conf(t)
     t.title = "__NAME__";
     t.version = "11.4";
-    t.console = true;
+    t.console = false; -- if console is active it wont open a new console if one is already open but will print to any other opened console (ie the console the parent love instance opened
     t.window.width = 800;
     t.window.height = 600;
     t.vSync = false;
